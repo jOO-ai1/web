141fdea06b408915fc03a21569a1c7cbcd07adde
jOO-ai1 <goyoussef96@gmail.com>
2025-07-20 21:18:12 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LangProvider } from './contexts/LangContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import { ToastProvider } from './contexts/ToastContext';
-import AppHeader from './components/AppHeader';
-import AppFooter from './components/AppFooter';
-import Home from './pages/Home';
-import ProductsPage from './pages/ProductsPage';
-import ProductPage from './pages/ProductPage';
-import CollectionPage from './pages/CollectionPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderConfirmation from './pages/OrderConfirmation';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AccountPage from './pages/AccountPage';
-import OrdersPage from './pages/OrdersPage';
-import OrderTrackingPage from './pages/OrderTrackingPage';
-import NotFoundPage from './pages/NotFoundPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import { useLang } from './contexts/LangContext';
-import { AnimatePresence } from 'framer-motion';
-
-function AppContent() {
-  const { lang } = useLang();
-  const safeLang = ["ar", "en"].includes(lang) ? lang : "en";
-
-  return (
-    <div dir={safeLang === "ar" ? "rtl" : "ltr"} className={safeLang === "ar" ? "font-arabic" : "font-montserrat"}>
-      <Router>
-        <AppHeader />
-        <div className="pt-28 min-h-[80vh] bg-app">
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<ProductsPage />} />
-              <Route path="/collections" element={<Home />} />
-              <Route path="/collections/:id" element={<CollectionPage />} />
-              <Route path="/product/:id" element={<ProductPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/checkout" element={
-                <ProtectedRoute>
-                  <CheckoutPage />
-                </ProtectedRoute>
-              } />
-              <Route path="/order-confirmation" element={<OrderConfirmation />} />
-              <Route path="/account" element={<AccountPage />} />
-              <Route path="/orders" element={<OrdersPage />} />
-              <Route path="/order-tracking" element={<OrderTrackingPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </AnimatePresence>
-        </div>
-        <AppFooter />
-      </Router>
-    </div>
-  );
-}
+import RoutesWrapper from './RoutesWrapper';
 
 export default function App() {
   return (
@@ -69,7 +13,7 @@ export default function App() {
         <AuthProvider>
           <CartProvider>
             <ToastProvider>
-              <AppContent />
+              <RoutesWrapper />
             </ToastProvider>
           </CartProvider>
         </AuthProvider>

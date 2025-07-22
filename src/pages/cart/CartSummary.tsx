557bdd8229f1539d8063b29d46fd0bed375a023a
jOO-ai1 <goyoussef96@gmail.com>
2025-07-22 23:37:25 +0300

    Fix FiCreditCard import in CartSummary

--- src/pages/cart/CartSummary.tsx
+++ src/pages/cart/CartSummary.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FiCreditCard } from 'react-icons/fi';
 import { useCart } from '../../contexts/CartContext';
 import { useLang, useTranslation } from '../../contexts/LangContext';
 import { COUPONS } from '../../constants/brand';
